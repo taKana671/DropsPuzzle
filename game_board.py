af2126907b302a9c9c2a8fb5a3b1176562945df8
taKana671 <kanae5321@gmail.com>
2023-07-29 00:14:45 +0900

    Changed the shape of game board.

--- game_board.py
+++ game_board.py
@@ -2,28 +2,28 @@ from itertools import product
 
 from panda3d.bullet import BulletRigidBodyNode
 from panda3d.bullet import BulletBoxShape
-from panda3d.bullet import BulletTriangleMeshShape, BulletTriangleMesh
+from panda3d.bullet import BulletTriangleMeshShape, BulletTriangleMesh, BulletConvexHullShape
 from panda3d.core import NodePath, PandaNode, CardMaker
 from panda3d.core import Vec3, Vec4, Point3, BitMask32, LColor
 from panda3d.core import TransparencyAttrib
 from panda3d.core import TransformState
 
-from create_geomnode import Cube
+from create_geomnode import Cube, RightTriangularPrism
 
 
-class Block(NodePath):
+# class Block(NodePath):
 
-    def __init__(self, name, geomnode, scale, hpr, pos):
-        super().__init__(BulletRigidBodyNode(name))
-        self.set_scale(scale)
-        self.set_pos(pos)
-        self.set_hpr(hpr)
-        self.block = geomnode.copy_to(self)
+#     def __init__(self, name, geomnode, scale, hpr, pos):
+#         super().__init__(BulletRigidBodyNode(name))
+#         self.set_scale(scale)
+#         self.set_pos(pos)
+#         self.set_hpr(hpr)
+#         self.block = geomnode.copy_to(self)
 
-        end, tip = self.block.get_tight_bounds()
-        shape = BulletBoxShape((tip - end) / 2)
-        self.node().add_shape(shape)
-        self.set_collide_mask(BitMask32.bit(1))
+#         end, tip = self.block.get_tight_bounds()
+#         shape = BulletBoxShape((tip - end) / 2)
+#         self.node().add_shape(shape)
+#         self.set_collide_mask(BitMask32.bit(1))
 
 
 class Assemblies(NodePath):
@@ -34,14 +34,15 @@ class Assemblies(NodePath):
         self.set_pos(pos)
         self.node().set_restitution(restitution)
 
-    def add_block(self, name, cube, scale, pos):
-        block = cube.copy_to(self)
+    def add_block(self, name, obj, pos, hpr):
+        block = obj.copy_to(self)
+        shape = BulletConvexHullShape()
+        shape.add_geom(block.node().get_geom(0))
+        self.node().add_shape(shape, TransformState.make_pos_hpr(pos, hpr))
+
         block.set_name(name)
-        block.set_scale(scale)
         block.set_pos(pos)
-        end, tip = block.get_tight_bounds()
-        shape = BulletBoxShape((tip - end) / 2)
-        self.node().add_shape(shape, TransformState.make_pos(pos))
+        block.set_hpr(hpr)
 
         return block
 
@@ -60,6 +61,22 @@ class Assemblies(NodePath):
         return card
 
 
+# class Card(NodePath):
+
+#     def __init__(self, name, rect, hpr, pos, bit):
+#         super().__init__(BulletRigidBodyNode(name))
+#         self.set_pos(pos)
+#         self.set_hpr(hpr)
+#         self.card = rect.copy_to(self)
+
+#         geom = self.card.node().get_geom(0)
+#         mesh = BulletTriangleMesh()
+#         mesh.add_geom(geom)
+#         shape = BulletTriangleMeshShape(mesh, dynamic=False)
+#         self.node().add_shape(shape)
+#         self.set_collide_mask(BitMask32.bit(bit))
+
+
 class GameBoard(NodePath):
 
     def __init__(self, world):
@@ -69,89 +86,38 @@ class GameBoard(NodePath):
 
     def create_game_board(self):
         body_color = LColor(0, 0.5, 0, 1)
-        cube = Cube()
-
-        # top
-        self.cover = Assemblies('cover', Point3(0, 0, 10))
-        self.create_cover(cube, self.cover, body_color)
-        self.cover.reparent_to(self)
-        self.world.attach(self.cover.node())
 
         # body
         self.body = Assemblies('body', Point3(0, 0, 0), 1.0)
-        self.create_body(cube, self.body, body_color)
-
-        # transparent panels
-        self.body.set_transparency(TransparencyAttrib.MAlpha)
-        tex = base.loader.load_texture('textures/panel.png')
-        self.create_panels(self.body, tex)
-
+        self.create_body(self.body, body_color)
         self.body.reparent_to(self)
         self.world.attach(self.body.node())
 
-        # feed pipe
-        self.pipe = Assemblies('pipe', Point3(0, 0, 12.5))
-        self.create_feed_pipe(self.pipe)
-        self.pipe.reparent_to(self)
-        self.world.attach(self.pipe.node())
-        self.pipe.hide()
-        self.flatten_strong()
+        # top
+        # self.cover = Assemblies('cover', Point3(0, 0, 10))
+        # self.create_cover(cube, self.cover, body_color)
+        # self.cover.reparent_to(self)
+        # self.world.attach(self.cover.node())
 
-    def create_cover(self, cube, cover, color):
+    def create_body(self, body, color):
         blocks = [
-            [Vec3(3, 10, 1), (Point3(x, 0, 0) for x in (3.5, -3.5))],
-            [Vec3(4, 3, 1), (Point3(0, y, 0) for y in (3.5, -3.5))],
+            [Cube(w=13, d=2, h=10), [(Point3(0, 0, -6), Vec3(0, 0, 0))]],
+            [Cube(w=0.5, d=2, h=22), [(Point3(6.25, 0, 2), Vec3(0, 0, 0)), (Point3(-6.25, 0, 2), Vec3(0, 0, 0))]],
+            [RightTriangularPrism(w=1.5, h=2), [(Point3(5.75, 0, -0.5), Vec3(180, 90, 0)), (Point3(-5.75, 0, -0.5), Vec3(0, 90, 0))]]
         ]
 
-        for i, (scale, pos_gen) in enumerate(blocks):
-            for j, pos in enumerate(pos_gen):
-                block = cover.add_block(f'cover_{i}{j}', cube, scale, pos)
+        for i, (obj, details) in enumerate(blocks):
+            for j, (pos, hpr) in enumerate(details):
+                block = body.add_block(f'body_{i}{j}', obj, pos, hpr)
                 block.set_color(color)
 
-    def create_body(self, cube, body, color):
-        blocks = [
-            [Vec3(10, 10, 10), [Point3(0, 0, -6)]],
-            [Vec3(0.25, 0.25, 11), (Point3(x, y, 4) for x, y in product((4.875, -4.875), repeat=2))]
-        ]
+    # def create_cover(self, cube, cover, color):
+    #     blocks = [
+    #         [Vec3(3, 10, 1), (Point3(x, 0, 0) for x in (3.5, -3.5))],
+    #         [Vec3(4, 3, 1), (Point3(0, y, 0) for y in (3.5, -3.5))],
+    #     ]
 
-        for i, (scale, pos_gen) in enumerate(blocks):
-            for j, pos in enumerate(pos_gen):
-                block = body.add_block(f'body_{i}{j}', cube, scale, pos)
-                block.set_color(color)
-
-    def create_panels(self, body, tex):
-        size = Vec4(-5, 5, -5.5, 5.5)
-        x, y, z = 4.99, 4.99, 4.0
-
-        panels = [
-            [Vec3(0, 0, 0), Point3(0, -y, z)],
-            [Vec3(180, 0, 0), Point3(0, y, z)],
-            [Vec3(-90, 0, 0), Point3(-x, 0, z)],
-            [Vec3(90, 0, 0), Point3(x, 0, z)]
-        ]
-
-        card = CardMaker('card')
-        card.set_frame(size)
-        rect = NodePath(card.generate())
-
-        for i, (hpr, pos) in enumerate(panels):
-            panel = body.add_card(f'panel_{i}', rect, hpr, pos)
-            panel.set_texture(tex)
-
-    def create_feed_pipe(self, pipe):
-        size = Vec4(-2, 2, -3, 3)
-        x, y, z = 2, 2, 0
-
-        panels = [
-            [Vec3(0, 0, 0), Point3(0, -y, z)],
-            [Vec3(180, 0, 0), Point3(0, y, z)],
-            [Vec3(-90, 0, 0), Point3(-x, 0, z)],
-            [Vec3(90, 0, 0), Point3(x, 0, z)]
-        ]
-
-        card = CardMaker('card')
-        card.set_frame(size)
-        rect = NodePath(card.generate())
-
-        for i, (hpr, pos) in enumerate(panels):
-            pipe.add_card(f'pipe_{i}', rect, hpr, pos)
+    #     for i, (scale, pos_gen) in enumerate(blocks):
+    #         for j, pos in enumerate(pos_gen):
+    #             block = cover.add_block(f'cover_{i}{j}', cube, scale, pos)
+    #             block.set_color(color)
