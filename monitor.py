4f3300971d5561782812c6dac6c508084013a22d
taKana671 <kanae5321@gmail.com>
2023-08-31 00:57:17 +0900

    First commit

---
+++ monitor.py
@@ -0,0 +1,35 @@
+from collections import deque
+
+from direct.interval.IntervalGlobal import Sequence
+from direct.showbase.ShowBaseGlobal import globalClock
+
+
+class WarningSequence(Sequence):
+
+    def __init__(self, np):
+        super().__init__(
+            np.colorScaleInterval(0.5, (0.5, 0.5, 0.5, 1.0)),
+            np.colorScaleInterval(0.5, (1.0, 1.0, 1.0, 1.0))
+        )
+
+
+class Monitor:
+
+    def __init__(self, game_board):
+        self.game_board = game_board
+        self.monitor_q = deque()
+
+    def monitoring(self):
+        try:
+            np = self.monitor_q.popleft()
+
+            if self.game_board.is_in_gameover_zone(np):
+                base.messenger.send('gameover')
+                WarningSequence(np).loop()
+                # return True
+        except IndexError:
+            pass
+
+    def start_monitoring(self, np, task):
+        self.monitor_q.append(np)
+        return task.done
